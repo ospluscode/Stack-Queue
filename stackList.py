72ef98082f78e93b72eeeae90d37f16bf4d099ed
Orkhan Suleymanov <53385236+ospluscode@users.noreply.github.com>
2021-03-12 20:20:20 -0800

    Add files via upload
    Stack implementations using Py List

---
+++ stackList.py
@@ -0,0 +1,42 @@
+# Implementing Stack using python Lists
+
+class Stack:
+    # create stack
+    def __init__(self):
+        self.list = []
+
+    # printing as string
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
+
+
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
+
+
+    def push(self, value):
+        self.list.append(value)
+        return "Element pushed"
+
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        else:
+            return self.list.pop()
+    
+
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        else:
+            return self.list[len(self.list)-1]
+    
+
+    def delete(self):
+        self.list = None
